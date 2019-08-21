aee03779038239d8f06212cd1d8ca06ea6bc01e5
js-jslog <jhs4jbs@hotmail.co.uk>
2019-08-21 10:37:19 +0000

    Add a constructor to the class

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-class Index {}
+class Index {
+  constructor(param1, param2) {
+    this.param1 = param1;
+    this.param2 = param2;
+  }
+}
 
 Index.staticFunction = () => 'this is a static function';
 
--- index.test.js
+++ index.test.js
@@ -13,3 +13,9 @@ test('Index contains a static function export', () => {
   expect(typeof Index.staticFunction).toBe('function');
   expect(Index.staticFunction()).toBe('this is a static function');
 });
+
+test('Index has a constructor which sets instance variables', () => {
+  const instance = new Index('one', 'two');
+  expect(instance.param1).toBe('one');
+  expect(instance.param2).toBe('two');
+});
